7905d891bea77797aae4664d04315e203a331143
samar151110 <92369339+samar151110@users.noreply.github.com>
2022-09-13 15:49:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+Status = "";
+objects = [];
+
+function setup(){
+     canvas = createCanvas(360,360);
+     canvas.center();
+
+     video = createCapture();
+     video.hide;
+}
+
+
+function start(){
+    object = ml5.objectDetector("cocossd", modelLoded);
+    document.getElementById("status").innerHTML = "status : Detecting objects";
+    document.getElementById("Object_name").value;
+}
+
+function draw(){
+    image(video, 0, 0, 360, 360);
+    
+}
+
+function gotresult(error, results) {
+    if (error) {
+         console.error(error);
+    }
+    else {
+         console.log(results);
+         objects = results;
+    }
+}
